a7234ea9ea2b2e1ecc129575378c684f61044aa9
Zhassulan Abishev <68692894+jotaro2020@users.noreply.github.com>
2021-01-22 13:04:54 +0600

    Update cart.js

--- src/screens/cart.js
+++ src/screens/cart.js
@@ -6,18 +6,9 @@ function CartScreen() {
   return (
     <ListItemList
       filterListItems={li => !li.finishDate}
-      noListItems={
-        <p>
-          Hey there! Welcome to your bookshelf reading list. Get started by
-          heading over to <Link to="/discover">the Discover page</Link> to add
-          books to your list.
-        </p>
-      }
       noFilteredListItems={
         <p>
-          Looks like you've finished all your books! Check them out in your{' '}
-          <Link to="/finished">finished books</Link> or{' '}
-          <Link to="/discover">discover more</Link>.
+          Карзина пуста
         </p>
       }
     />
